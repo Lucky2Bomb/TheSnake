cb5197faf36894b5f355303738daf6e9d1962d91
Lucky2Bomb <62508316+Lucky2Bomb@users.noreply.github.com>
2020-03-29 23:56:44 +0500

    alpha version 0.01
    implemented:
    + snake management
    + eating an object
    + increase the length of the snake when eating an object
    + teleportation of the snake on the walls
    not implemented:
    - collision of a snake with its own body
    - game interface

--- script.js
+++ script.js
@@ -3,68 +3,88 @@
 let snake_x = 0;
 let snake_y = 0;
 const pixel_size = 20;
-let snake_step = pixel_size;
-const snake_step_interval = 200;
+let snakeStep = pixel_size;
+const snakeStepInterval = 200;
 
 let canvas = document.getElementById('area');
 let context = canvas.getContext('2d');
 let img_snake = new Image();
 img_snake.src = "snake.jpg";
-let canvas_width = canvas.width;
-let canvas_height = canvas.height;
+let canvasWidth = canvas.width;
+let canvasHeight = canvas.height;
 
 let target_x = 0;
 let target_y = 0;
 
-// console.log("canvas width:" + canvas_width);
-// console.log("canvas height:" + canvas_height);
+// console.log("canvas width:" + canvasWidth);
+// console.log("canvas height:" + canvasHeight);
 
 let w = false;
 let a = false;
 let s = false;
 let d = false;
 
-
-
-let pX = [];
-let pY = [];
+let currentSnakeTailX = [];
+let currentSnakeTailY = [];
 
 snake_x = SetRandomPosX();
 snake_y = SetRandomPosY();
 target_x = SetRandomPosX();
 target_y = SetRandomPosY();
 
-// console.log("snake_x: " + snake_x + "; snake_y: " + snake_y);
+setLengthSnake(4);
+
 drawFrame();
-// pX = snake_x;
-// pY = snake_y;
+
+window.setInterval(clearXBOCT, snakeStepInterval);
 
 
 window.addEventListener("keydown", window_keyDown);
-window.addEventListener("keydown", window_cutOffTheTail);
+window.addEventListener("keydown", checkSnakeCrush_keyDown);
 
-function window_cutOffTheTail() {
-    if (window.event.keyCode == 87 || window.event.keyCode == 65 || window.event.keyCode == 83 || window.event.keyCode == 68) {
-        window.removeEventListener("keydown", window_cutOffTheTail);
-        setTimeout(cutTail, 200);
+function setLengthSnake(value) {
+    for(let i = 0; i < value; i++ ) {
+        currentSnakeTailX.push(snake_x);
+        currentSnakeTailY.push(snake_y);
     }
+}
 
+
+// window.addEventListener("keydown", window_cutOffTheTail);
+
+function checkSnakeCrush_keyDown() {
+    //console.log(window.event.keyCode);
+    if (window.event.keyCode == 87 || window.event.keyCode == 65 || 
+        window.event.keyCode == 83 || window.event.keyCode == 68) { 
+        setTimeout(checkSnakeCrush, snakeStepInterval);
+        window.removeEventListener("keydown", checkSnakeCrush_keyDown);
+     }
 }
 
 function window_keyDown() {
-    // console.log(window.event.keyCode);
+    //console.log(window.event.keyCode);
     if (window.event.keyCode == 87) { this.W_keyDown(); }
     if (window.event.keyCode == 65) { this.A_keyDown(); }
     if (window.event.keyCode == 83) { this.S_keyDown(); }
     if (window.event.keyCode == 68) { this.D_keyDown(); }
 
     //space
-    if (window.event.keyCode == 32) { window.setInterval(clearXBOCT, snake_step_interval); }
+    if (window.event.keyCode == 32) { window.setInterval(clearXBOCT, snakeStepInterval); }
 
+    //alt
+    if (window.event.keyCode == 18) {
+        console.log("snake crush!");
+        for(let i = 0; i < currentSnakeTailX.length; i++ ) {
+            console.log(`X[${i}] ` + currentSnakeTailX[i] + `; Y[${i}] ` + currentSnakeTailY[i]);
+        }
+        console.log("current snake x = " + snake_x + "; current snake y = " + snake_y);
+        alert("oh!");
+    }
+    
     //ctrl
     if (window.event.keyCode == 17) {
-        pX.push(pX[pX.length - 1]);
-        pY.push(pY[pY.length - 1]);
+        currentSnakeTailX.push(currentSnakeTailX[currentSnakeTailX.length - 1]);
+        currentSnakeTailY.push(currentSnakeTailY[currentSnakeTailY.length - 1]);
     }
 }
 
@@ -74,93 +94,90 @@ function attackTheTarget() {
         target_x = SetRandomPosX();
         target_y = SetRandomPosY();
 
-        pX.push(pX[pX.length - 1]);
-        pY.push(pY[pY.length - 1]);
+        currentSnakeTailX.push(currentSnakeTailX[currentSnakeTailX.length - 1]);
+        currentSnakeTailY.push(currentSnakeTailY[currentSnakeTailY.length - 1]);
     }
-}
-
-function cutTail() {
-    window.setInterval(clearXBOCT, snake_step_interval);
 }
 
 function drawFrame() {
     // context.clearRect(snake_x, snake_y, 20, 20);
     context.beginPath();
 
-
-
-
     // context.fillRect(snake_x, snake_y, pixel_size, pixel_size);
-
 
     context.fillStyle = "#900000";
     context.fillRect(target_x, target_y, pixel_size, pixel_size);
     context.drawImage(img_snake, snake_x, snake_y, pixel_size, pixel_size);
 
     context.stroke();
-
     goMove();
+    // checkSnakeCrush();
 
-    if (snake_x >= canvas_width) {
-        snake_x = 0;
-    }
-
-    if (snake_y >= canvas_height) {
-        snake_y = 0;
-    }
-
-    if (snake_x < 0) {
-        snake_x = canvas_width - pixel_size;
-    }
-
-    if (snake_y < 0) {
-        snake_y = canvas_height - pixel_size;
-    }
+    teleportOutOfWall();
 
     attackTheTarget();
 
-    pX.push(snake_x);
-    pY.push(snake_y);
+    setTimeout(drawFrame, snakeStepInterval);
 
-
-
-    setTimeout(drawFrame, snake_step_interval);
+    currentSnakeTailX.push(snake_x);
+    currentSnakeTailY.push(snake_y);
 }
 
 function SetRandomPosX() {
-    return getRandomInt(2, canvas_width / pixel_size) * pixel_size - pixel_size;
+    return getRandomInt(2, canvasWidth / pixel_size) * pixel_size - pixel_size;
 }
 
 function SetRandomPosY() {
-    return getRandomInt(2, canvas_height / pixel_size) * pixel_size - pixel_size;
+    return getRandomInt(2, canvasHeight / pixel_size) * pixel_size - pixel_size;
 }
 
 function clearXBOCT() {
-    context.clearRect(pX.shift(), pY.shift(), pixel_size, pixel_size);
+    context.clearRect(currentSnakeTailX.shift(), currentSnakeTailY.shift(), pixel_size, pixel_size);
 }
-
-
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function checkSnakeCrush() {
+    if (currentSnakeTailX.indexOf(snake_x) > 0 && currentSnakeTailY.indexOf(snake_y) > 0) {
+        if(currentSnakeTailX.indexOf(snake_x) == currentSnakeTailY.indexOf(snake_y)){
+        console.log("snake crush!");
+        setTimeout(checkSnakeCrush, snakeStepInterval);
+    }
+}
+}
+
+function teleportOutOfWall() {
+    if (snake_x >= canvasWidth) {
+        snake_x = 0;
+    }
+
+    if (snake_y >= canvasHeight) {
+        snake_y = 0;
+    }
+
+    if (snake_x < 0) {
+        snake_x = canvasWidth - pixel_size;
+    }
+
+    if (snake_y < 0) {
+        snake_y = canvasHeight - pixel_size;
+    }
+}
+
 function goMove() {
     if (w) {
-        snake_y -= snake_step;
-        // context.clearRect(snake_x, snake_y+60, 20, 20);
+        snake_y -= snakeStep;
     }
     if (a) {
-        snake_x -= snake_step;
-        // context.clearRect(snake_x+60, snake_y, 20, 20);
+        snake_x -= snakeStep;
     }
     if (s) {
-        snake_y += snake_step;
-        // context.clearRect(snake_x, snake_y-60, 20, 20);
+        snake_y += snakeStep;
     }
     if (d) {
-        snake_x += snake_step;
-        // context.clearRect(snake_x-60, snake_y, 20, 20);
+        snake_x += snakeStep;
     }
 }
 
